4861c56ef396b8da0ce115a2bed5f1aefd439060
Varga Rebeka <rebeccca06@gmail.com>
2024-11-24 14:42:21 +0100

    Hibák javitása a main.cpp-ben

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[N_ELEMENTS]; //Hiba az, hogy nem így kell írni [NELEMENTS]
+    std::cout << '1-100 ertekek duplazasa'<< std::endl;// Hianyzott <<std::endl és a pontosvessző
+    for (int i = 0; i < N_ELEMENTS; i++)//hibásan volt meg adva a kezdő érték
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++) // itt is hibásan volt megadva
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"<< b[i] << std::endl; //hiányzott, hogy mit szeretnénk kiírni, illetve std::endl; vég
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS; i++) // vessző helyett pontosvessző kell 
     {
-        atlag += b[i]
+        atlag += b[i]; //hiányzott pontosvessző
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    delete[] b; //memoria felszabadítása
     return 0;
 }
